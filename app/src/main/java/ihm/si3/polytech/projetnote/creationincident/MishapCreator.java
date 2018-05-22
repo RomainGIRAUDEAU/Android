5b1dca4583371e0e61a880d84dcf156a79929f7f
CORDIER <tristan.cordier@etu.unice.fr>
2018-05-23 00:35:51 +0200

    gps on continue

--- app/src/main/java/ihm/si3/polytech/projetnote/creationincident/MishapCreator.java
+++ app/src/main/java/ihm/si3/polytech/projetnote/creationincident/MishapCreator.java
@@ -1,6 +1,7 @@
 package ihm.si3.polytech.projetnote.creationincident;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
